ea1421a32a556b0b7c9f987b55c79724b5b10e2d
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2023-09-11 18:34:25 +0200

    Added pseudocode before main code

---
+++ scripts/javascript.js
@@ -0,0 +1,12 @@
+// Pseudocode / Algoritmo
+// Inizializzare funzione getComputerChoice e dare valore random "Rock", "Paper", "Scissors"
+// Usare math.random per definire valore variabile
+// Inizializzare variabile playerSelection
+// Inizializzare variabile computerSelection
+// Usare prompt per prendere la scelta del giocatore
+// Creare funzione che emula una partita di sasso carta forbice con i due parametri inizializzati
+// Ritornare (Return) una stringa che dichiara il vincitore (nella funzione)
+// Creare funzione game() per loopare 5 round
+// Mostrare il vincitore a fine gioco
+// 
+
