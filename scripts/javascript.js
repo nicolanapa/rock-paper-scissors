38ed774036e2d9d0eedd722954050103b1937789
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2023-09-27 19:49:00 +0200

    Removed calcoli loops and variables for the calculation of the winner

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -28,112 +28,48 @@ function getComputerChoice() {
 }
 getComputerChoice();
 
-function testPlay(computerSelection) {
+function testPlay() {
+    getComputerChoice();
     let risultati;
-    playerSelection = prompt("Choose between: Rock, Paper, Scissors", "");
+    playerSelection = prompt("Choose between: Rock, Paper, Scissors", "Rock");
     playerSelection = playerSelection.toLowerCase();
     if ( playerSelection == "rock" ) {
         if ( computerSelection == "rock" ) {
             risultati = "Oh! Draw between rock and rock!";
-            calcoli1 = 0;
         }
         else if ( computerSelection == "paper" ) {
             risultati = "Oh! Lost to the power of the Paper.";
-            calcoli1 = -1;
         }
         else if ( computerSelection == "scissors" ) {
             risultati = "Oh! Won against the poor Scissors!";
-            calcoli1 = +1;
         }
     }
     else if ( playerSelection == "paper" ) {
         if ( computerSelection == "paper" ) {
             risultati = "Oh! Draw between Paper and Paper!";
-            calcoli1 = 0;
         }
         else if ( computerSelection == "rock" ) {
             risultati = "Oh! Won against the poor Rock!";
-            calcoli1 = +1;
         }
         else if ( computerSelection == "scissors" ) {
             risultati = "Oh! Lost to the power of the Scissors.";
-            calcoli1 = -1;
         }
     }
     else if ( playerSelection == "scissors" ) {
         if ( computerSelection == "scissors" ) {
             risultati = "Oh! Draw between Scissors and Scissors!";
-            calcoli1 = 0;
         }
         else if ( computerSelection == "rock" ) {
             risultati = "Oh! Lost to the power of the Rock.";
-            calcoli1 = -1;
         }
         else if ( computerSelection == "paper" ) {
             risultati = "Oh! Won against the poor Paper!";
-            calcoli1 = +1;
         }
     }
     else {
         risultati = "Sorry not a option";
     }
-    return risultati, calcoli1;
-}
-// console.log(testPlay(computerSelection));
-
-function calcoliFunzione(calcoli1) {
-    calcoli;
-    calcoli1;
-    if ( calcoli1 == 0 ) {
-        calcoli = calcoli + 0;
-    }
-    else if ( calcoli1 == +1 ) {
-        calcoli = calcoli + 1;
-    }
-    else if ( calcoli1 == -1 ) {
-        calcoli = calcoli - 1;
-    }
+    return risultati;
 }
 
-function game() {
-    let calcoli1;
-    getComputerChoice();
-    console.log(getComputerChoice());
-    testPlay(computerSelection);
-    //calcoliFunzione()
-    getComputerChoice();
-    console.log(getComputerChoice());
-    testPlay(computerSelection);
-    //calcoliFunzione()
-    getComputerChoice();
-    console.log(getComputerChoice());
-    testPlay(computerSelection);
-    //calcoliFunzione()
-    getComputerChoice();
-    console.log(getComputerChoice());
-    testPlay(computerSelection);
-    //calcoliFunzione()
-    getComputerChoice();
-    console.log(getComputerChoice());
-    testPlay(computerSelection);
-    //calcoliFunzione()
-    getComputerChoice();
-    console.log(getComputerChoice());
-    /*if ( calcoli1 >= 5 ) {
-        console.log("And the winner is....  YOU! 5 - 0");
-    }
-    else if ( calcoli1 <= -5 ) {
-        console.log("And the winner is...  THE MACHINE! 0 - 5");
-    }
-    else if ( calcoli1 == 0 ) {
-        console.log("And the winner is...  DRAW! 0 - 0");
-    }
-    else if ( calcoli1 > 0 ) {
-        console.log("And the almost winner is...  YOU! > 0");
-    }
-    else if ( calcoli1 < 0 ) {
-        console.log("And the almost winner is...  THE MACHINE! < 0");
-    }
-    return calcoli1*/
-}
-console.log(game());
+console.log(testPlay());
